3af32bc3c5ccc382f7c40493d54558ac769ca199
coskunergan <coskunergan@gmail.com>
2020-12-23 18:06:23 +0300

    V1.0.1
    -Add loader data array.

--- ArduLoader.ino
+++ ArduLoader.ino
@@ -1,4 +1,6 @@
 
+#include <stdio.h>
+#include <FastGPIO.h>
 #include <CRC32.h>
 #include "TimerOne.h"
 
@@ -6,6 +8,24 @@
 
 #define FAIL_BLINK_PERIDOD  5 // 50mS
 #define LOAD_BLINK_PERIDOD  50 // 500mS
+
+#define CLK_PIN  2
+#define DTA_PIN  3
+#define VCC_PIN  4
+
+#define CLK_HIGH()  (FastGPIO::Pin<CLK_PIN>::setOutputValueHigh())
+#define CLK_LOW()  (FastGPIO::Pin<CLK_PIN>::setOutputValueLow())
+
+#define DTA_HIGH()  (FastGPIO::Pin<DTA_PIN>::setOutputValueHigh())
+#define DTA_LOW()  (FastGPIO::Pin<DTA_PIN>::setOutputValueLow())
+
+#define VCC_HIGH()  (FastGPIO::Pin<VCC_PIN>::setOutputValueHigh())
+#define VCC_LOW()  (FastGPIO::Pin<VCC_PIN>::setOutputValueLow())
+
+#define CLK_TOGGLE() (FastGPIO::Pin<CLK_PIN>::setOutputValueToggle())
+#define DTA_TOGGLE() (FastGPIO::Pin<DTA_PIN>::setOutputValueToggle())
+
+#define NOP()  __asm__ __volatile__ ("nop\n\t");
 
 CRC32 crc;
 
@@ -18,6 +38,12 @@ typedef enum
 } Led_State_t;
 
 //---------------------------------
+
+extern const PROGMEM char Array[];
+
+
+const char Array2[] PROGMEM = "0101010101010";
+
 uint8_t incomingByte;
 uint8_t UartTimeout;
 uint8_t LedTimeout;
@@ -73,9 +99,63 @@ void ISR_Time_Tick(void) // per 10mS
   //-----------------------
 }
 /***********************************************************/
+void SendPreamble(void)
+{
+  uint8_t i, j;
+
+  for (i = 0; i < 250; i++)
+  {
+    DTA_TOGGLE(); // 750Hz
+    for (j = 0; j < 59; j++)
+    {
+      CLK_TOGGLE();
+      delayMicroseconds(12); // 45KHz
+    }
+    VCC_HIGH();
+  }
+  DTA_LOW();
+  CLK_LOW();
+}
+/***********************************************************/
+void SendArray(void)
+{
+  int i;
+  char character;
+  do
+  {
+    CLK_LOW();
+    character = pgm_read_byte_near(Array + i);
+    i++;
+    if (character == '0')
+    {
+      DTA_LOW();
+      NOP();
+      CLK_HIGH();
+    }
+    else if (character  == '1')
+    {
+      DTA_HIGH();
+      NOP();
+      CLK_HIGH();
+    }
+    else
+    {
+      DTA_LOW();
+      CLK_LOW();
+      delayMicroseconds(10);
+    }
+    NOP();
+    NOP();
+  } while (character != '\0');
+
+}
+/***********************************************************/
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(CLK_PIN, OUTPUT);
+  pinMode(DTA_PIN, OUTPUT);
+  pinMode(VCC_PIN, OUTPUT);
   Serial.begin(115200);
   Serial.println(F("Restart!"));
 
@@ -87,6 +167,14 @@ void setup()
   LedTimeout = 0;
   TimeoutFlag = false;
   Led_State = LED_OFF;
+
+  VCC_LOW();
+
+  delay(1000);
+  SendPreamble();
+  SendArray();
+  delay(500);
+  VCC_LOW();
 }
 /***********************************************************/
 void loop()
@@ -108,27 +196,27 @@ void loop()
 /***********************************************************/
 void serialEvent()
 {
-  while(Serial.available())
-  {    
+  while (Serial.available())
+  {
     incomingByte = Serial.read();
     UartTimeout = UART_TIMEOUT;
     Led_State = LED_LOAD;
     switch (bytes++)
     {
-        case 0:
-          crc32 = incomingByte;
-          break;
-        case 1:
-          crc32 |= ((uint32_t)incomingByte << 8);
-          break;
-        case 2:
-          crc32 |= ((uint32_t)incomingByte << 16);
-          break;
-        case 3:
-          crc32 |= ((uint32_t)incomingByte << 24);
-          crc.reset();
-          break;
-      default:        
+      case 0:
+        crc32 = incomingByte;
+        break;
+      case 1:
+        crc32 |= ((uint32_t)incomingByte << 8);
+        break;
+      case 2:
+        crc32 |= ((uint32_t)incomingByte << 16);
+        break;
+      case 3:
+        crc32 |= ((uint32_t)incomingByte << 24);
+        crc.reset();
+        break;
+      default:
         crc.update(incomingByte);
         break;
     }
